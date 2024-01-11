2f92070a9498a3ef39e9a28575b5e609c3d24f39
kamekamepon2 <146316520+kamekamepon2@users.noreply.github.com>
2024-01-12 03:35:13 +0900

    correct redis port

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ RabbitMQUser=args[2]
 RabbitMQPassword=args[3]
 TEAMS_WEB_HOOK_URL=args[4]
 RedisHost="redis-12413.c54.ap-northeast-1-2.ec2.cloud.redislabs.com"
-RedisPort=":2413"
+RedisPort="12413"
 RedisPwd="6NCxHeoTSYJtRbfN4Nis8ui5EN5wK2gQ"
 
 
